ab4db9ee70a476eca5c8ebb82186739f87bd3a42
batnyam9911 <gegee9933@gmail.com>
2021-05-28 10:05:15 +0800

    Ner soliloo

--- components/intro.js
+++ components/intro.js
@@ -11,10 +11,9 @@ export default () => (
       alt="Generic placeholder"
     />
     <Media.Body>
-      <h5 className="font-weight-bold mb-0">1234.mn - Онлайн видео сургалт</h5>
+      <h5 className="font-weight-bold mb-0">ХӨВСГӨЛ ЗАР</h5>
       <p className="welcome-text">
-        Бид програмчлалын технолгийн чиглэлээр төрөл бүрийн сонирхолтой
-        мэдээллүүдийг энэхүү блогоор хүргэж байна.
+        Хөвсгөлийн анхны зарын сайт.
       </p>
     </Media.Body>
   </Media>
